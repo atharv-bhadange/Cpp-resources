d20b50787a72941fbe463f0abefd29e3b708ee03
atharv-bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2021-10-08 09:36:15 +0530

    Updated highestpowerof2 function

--- Bit_manipulation.cpp
+++ Bit_manipulation.cpp
@@ -1,62 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool ispowerof2(int n){
-    //To get n-1 from n in binary form, filpping the bits after rightmost set bit(1) would give n-1 
-    //so if n is power of 2 then it's & with n-1 will be zero
-          //'n &&' is done to treat corner case of 0
-    return (n && !(n & n-1));
+int highestPowerof2(int x) //highest power of 2 less than n
+{
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+
+    return x ^ (x >> 1);
 }
 
-int onesinnumber(int n){
+bool ispowerof2(int n)
+{
+    //To get n-1 from n in binary form, filpping the bits after rightmost set bit(1) would give n-1
+    //so if n is power of 2 then it's & with n-1 will be zero
+    //'n &&' is done to treat corner case of 0
+    return (n && !(n & n - 1));
+}
+
+int onesinnumber(int n)
+{
     // (n & n-1) gives same bits as n except the rightmost setbit
 
     int count = 0;
-    while(n != 0)
+    while (n != 0)
     {
-        n = n & (n-1);
+        n = n & (n - 1);
         count++;
     }
     return count;
 }
 
-void unique_ele(int n){
+void unique_ele(int n)
+{
     int arr[n];
     int xorus = 0;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
         xorus = xorus ^ arr[i]; //same elements cancel out each other
     }
 
-    cout<<xorus<<endl;
+    cout << xorus << endl;
 }
 
-void two_unique_eles(int n){
+void two_unique_eles(int n)
+{
     int arr[n];
     int xorus = 0;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
         xorus = xorus ^ arr[i];
     }
     int sb = 0;
     int pos = 0;
-    while(sb != 1)
+    while (sb != 1)
     {
         sb = xorus & 1; //give the rightmost bit in binary representation of number
         pos++;
         xorus >>= 1;
     }
-    
 }
 
-int main(){
-    int n; cin>>n;
+int main()
+{
+    int n;
+    cin >> n;
     //Q. Write a program to check if a number is power of 2
     cout << ispowerof2(n) << endl;
 
-    //Q. Write a program to count number of 1s in binary reprsentation of a number 
+    //Q. Write a program to count number of 1s in binary reprsentation of a number
     cout << onesinnumber(n) << endl;
 
     //Q. Write a program to find a unique number in an array where all numbers except one, are present twice.
